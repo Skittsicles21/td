0ac5da3de4f172d5e16a20cacd07d62eaa61f9ca
Skittsicles21 <jacobs.junk1234@gmail.com>
2023-03-25 00:28:35 -0400

    Place Tile

--- My project (1)/Assets/Scripts/LevelManager.cs
+++ My project (1)/Assets/Scripts/LevelManager.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-
+    //A prefab for creating a single tile
     [SerializeField]
     private GameObject tile;
+
+    //calculates the tile width and returns the value as a float
+    public float TileSize
+    {
+        get { return tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        //Activates the create level function
         CreateLevel();
     }
 
@@ -19,9 +26,25 @@ public class LevelManager : MonoBehaviour
     {
         
     }
-
+    //Creates the level
     private void CreateLevel() 
     {
-        Instantiate(tile);
+
+        for (int y = 0; y < 5; y++) //y position
+        {
+            for (int x = 0; x < 5; x++) //x position
+            {
+                PlaceTile(x, y);
+            }
+        }
+    }
+
+    private void PlaceTile(int x, int y)
+    {
+        //Creates a new tile and then makes a reference to that tile in the newTile variable
+        GameObject newTile = Instantiate(tile);
+
+        //Uses the newtile variable and the tile width to place a the next tile right next to the last
+        newTile.transform.position = new Vector3(TileSize * x, TileSize * y, 0);
     }
 }
