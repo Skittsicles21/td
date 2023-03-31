a847ad883f7263ecc8318cc685a47926bda1687d
Skittsicles21 <jacobs.junk1234@gmail.com>
2023-03-31 19:54:46 -0400

    Grid position + Tile dictionary
    The grid positions will allow us to place things like towers on the tiles.

--- My project (1)/Assets/Scripts/LevelManager.cs
+++ My project (1)/Assets/Scripts/LevelManager.cs
@@ -9,9 +9,12 @@ public class LevelManager : MonoBehaviour
     [SerializeField]
     private GameObject[] tilePrefabs;
 
+    public Dictionary<Point, TileScript> Tiles {get; set;}
+
     //calculates the tile width and returns the value as a float
     public float TileSize
     {
+        //
         get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
     }
 
@@ -27,14 +30,18 @@ public class LevelManager : MonoBehaviour
     {
         
     }
+
     //Creates the level
     private void CreateLevel() 
     {
+        Tiles = new Dictionary<Point, TileScript>();
 
         string[] mapData = ReadLevelText();
 
         int mapX = mapData[0].ToCharArray().Length;
         int mapY = mapData.Length;
+
+        Vector3 maxTile = Vector3.zero;
 
         Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
 
@@ -47,6 +54,8 @@ public class LevelManager : MonoBehaviour
                 PlaceTile(newTiles[x].ToString(), x, y, worldStart);
             }
         }
+
+        maxTile = Tiles[new Point(mapX-1, mapY-1)].transform.position;
     }
 
     private void PlaceTile(string tileType, int x, int y, Vector3 worldStart)
@@ -54,10 +63,12 @@ public class LevelManager : MonoBehaviour
         int tileIndex = int.Parse(tileType);
 
         //Creates a new tile and then makes a reference to that tile in the newTile variable
-        GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
+        TileScript newTile = Instantiate(tilePrefabs[tileIndex]).GetComponent<TileScript>();
 
         //Uses the newtile variable and the tile width to place a the next tile right next to the last
-        newTile.transform.position = new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0);
+        newTile.Setup(new Point(x, y), new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0));
+
+        Tiles.Add(new Point(x,y), newTile);
     }
 
     private string[] ReadLevelText()
@@ -69,8 +80,6 @@ public class LevelManager : MonoBehaviour
         return data.Split('-');
     }
 }
-
-
 
 
 
